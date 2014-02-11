34d1ce838a8955f99bad7243521b7b758f141403
silver <keepishop@penny.home.agmccollum.com>
2014-02-10 18:37:15 -0800

    It firefoxes by itself! --Andrew

--- lib/spunkmeyer/chrome.rb
+++ lib/spunkmeyer/chrome.rb
@@ -9,18 +9,44 @@ module Spunkmeyer
       when :osx
         "#{Dir.home}/Library/Application Support/Google/Chrome/Default/Cookies"
       when :linux
-        "#{Dir.home}/.config/google-chrome/Default/Cookies"
+      case browser 
+        when :chrome 
+          "#{Dir.home}/.config/google-chrome/Default/Cookies"
+        when :firefox 
+          Dir.glob("#{Dir.home}/.mozilla/firefox/*.default/cookies.sqlite").first
+        end
       else
         raise 'Spunkmeyer::Chrome doesn\'t know this operating system.'
+      end
+    end
+
+    def self.browser
+      case Spunkmeyer.os
+      when :osx
+        :chrome
+      when :linux
+        if File.exists?("#{Dir.home}/.config/google-chrome/Default/Cookies")
+          :chrome
+        elsif File.exists?(Dir.glob("#{Dir.home}/.mozilla/firefox/*.default/cookies.sqlite").first)
+          :firefox
+        end 
       end
     end
 
     def self.cookies(domain)
       hk = host_key domain
       db = SQLite3::Database.open cookie_path
-      fields = [ :host_key, :path, :name, :value, :expires_utc ]
-      sql_fields = fields.map { |f| f.to_s }.join(', ')
-      rows = db.execute "select #{sql_fields} from cookies where host_key like '%#{hk}%'"
+      case browser
+      when :firefox
+        fields = [ :host, :path, :name, :value, :expiry ]
+        sql_fields = fields.map { |f| f.to_s }.join(', ')
+        rows = db.execute "select #{sql_fields} from moz_cookies where host like '%#{hk}%'"
+        fields = [ :host_key, :path, :name, :value, :expires_utc ]
+      when :chrome
+        fields = [ :host_key, :path, :name, :value, :expires_utc ]
+        sql_fields = fields.map { |f| f.to_s }.join(', ')
+        rows = db.execute "select #{sql_fields} from cookies where host_key like '%#{hk}%'"
+      end
       rows.map! { |r| Hash[fields.zip r] }
       names = rows.map { |r| r[:name] }
       Hash[names.zip rows]
